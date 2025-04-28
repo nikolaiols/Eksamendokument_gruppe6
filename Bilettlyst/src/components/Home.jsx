720567c0f50398b797cf016654dd7ec87832ade1
nikolaiols <nikolaio@hiof.no>
2025-04-29 00:15:48 +0200

    fått frem navn på home. og hentet all info om de forskjellige

--- Bilettlyst/src/components/Home.jsx
+++ Bilettlyst/src/components/Home.jsx
@@ -1,6 +1,75 @@
+import { useEffect, useState } from "react";
+
 export default function Home(){
+  
+ const [Findings, setFindings] = useState([]);
+ const [Tons, setTons] = useState([]);
+ const [Skei, setSkei] = useState([]);
+ const [Ne, setNeon] = useState([]);
+
+
+//findings
+ const getFindings = async () => {
+   fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&keyword=findings&locale=*&countryCode=NO") 
+     .then((response) => response.json()) 
+     .then((data) => setFindings(data._embedded?.events))
+     .catch((error) => console.error("Skjedde noe feil ved fetch", error)); 
+ };
+
+ //Tons of rock
+ const getTons = async () => {
+    fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&keyword=tons%20of%20rock&locale=*&countryCode=NO")
+      .then((response) => response.json()) 
+      .then((data) => setTons(data._embedded?.events)) 
+      .catch((error) => console.error("Skjedde noe feil ved fetch", error)); 
+  };
+
+  //skeikampenfestivalen
+ const getSkei = async () => {
+    fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&keyword=skeikampenfestivalen&locale=*&countryCode=NO")
+      .then((response) => response.json()) 
+      .then((data) => setSkei(data._embedded?.events)) 
+      .catch((error) => console.error("Skjedde noe feil ved fetch", error)); 
+  };
+
+  //NEON
+  const getNeon = async () => {
+    fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&keyword=neon&locale=*&countryCode=NO")
+      .then((response) => response.json()) 
+      .then((data) => setNeon(data._embedded?.events)) 
+      .catch((error) => console.error("Skjedde noe feil ved fetch", error)); 
+  };
+ 
+  useEffect(() => {
+   getFindings()
+   getTons()
+   getSkei()
+   getNeon()
+   console.log("Findings:", Findings);
+   console.log("Tons of Rock:", Tons); 
+   console.log("skeikampenfestivalen:", Skei);
+   console.log("NEON:", Ne);
+ }, []); 
+
+    
     return(
-        <h1>HOME</h1>
+        <>
+        {Findings.map((fin) => (
+            <h3>{fin.name}</h3>
+        ))}
+
+        {Tons.map((ton) => (
+            <h3>{ton.name}</h3>
+        ))}
+
+        {Skei.map((skei) => (
+            <h3>{skei.name}</h3>
+        ))}
+
+        {Ne.map((ne) => (
+            <h3>{ne.name}</h3>
+        ))}
+        </>
     )
 
 }
