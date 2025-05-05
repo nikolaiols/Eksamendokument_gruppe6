80d02438f9dc43925eb0aa9f7d475b9919b36e58
nikolaiols <nikolaio@hiof.no>
2025-05-05 10:50:11 +0200

    endret fetch

--- Bilettlyst/src/components/Home.jsx
+++ Bilettlyst/src/components/Home.jsx
@@ -10,47 +10,18 @@ export default function Home(){
   };
   
   // Lager en "state" (en variabel som React husker og kan oppdatere) for events
- const [Findings, setFindings] = useState([]);
- const [Tons, setTons] = useState([]);
- const [Skei, setSkei] = useState([]);
- const [Ne, setNeon] = useState([]);
+ const [Festival, setFestival] = useState([]);
  const [City, setCity] = useState([]);
 
 
 //findings
- const getFindings = async () => {
-   fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&keyword=findings&locale=*&size=1&countryCode=NO") // Henter data fra API-et.
+ const getFestival = async () => {
+   fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&id=Z698xZb_Z16vfkqIjU,%20Z698xZb_Z16v7eGkFy,%20Z698xZb_Z17qfaA,%20Z698xZb_Z17q339&locale=*") // Henter data fra API-et.
      .then((response) => response.json()) //gjør om til JSON-format
-     .then((data) => setFindings(data._embedded?.events)) // Setter Findings i state-variabelen
+     .then((data) => setFestival(data._embedded?.events)) // Setter Findings i state-variabelen
      .catch((error) => console.error("Skjedde noe feil ved fetch", error)); //feilmeldinger
  };
 
- // https://app.ticketmaster.com/discovery/v2/attractions?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&id=K8vZ917oWOV,%20K8vZ917K7fV,%20K8vZ917_YJf,%20K8vZ917bJC7&locale=*
-
- 
- //Tons of rock
- const getTons = async () => {
-    fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&keyword=tons%20of%20rock&locale=*&size=1&countryCode=NO")
-      .then((response) => response.json()) 
-      .then((data) => setTons(data._embedded?.events)) 
-      .catch((error) => console.error("Skjedde noe feil ved fetch", error)); 
-  };
-
-  //skeikampenfestivalen
- const getSkei = async () => {
-    fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&keyword=skeikampenfestivalen&locale=*&size=1&countryCode=NO")
-      .then((response) => response.json()) 
-      .then((data) => setSkei(data._embedded?.events)) 
-      .catch((error) => console.error("Skjedde noe feil ved fetch", error)); 
-  };
-
-  //NEON
-  const getNeon = async () => {
-    fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&keyword=neon&locale=*&size=1&countryCode=NO")
-      .then((response) => response.json()) 
-      .then((data) => setNeon(data._embedded?.events)) 
-      .catch((error) => console.error("Skjedde noe feil ved fetch", error)); 
-  };
 
     //city
     const getCity = async (text) => {
@@ -62,15 +33,9 @@ export default function Home(){
 
  
   useEffect(() => {
-   getFindings()
-   getTons()
-   getSkei()
-   getNeon()
+   getFestival()
    //getCity()
-   console.log("Findings:", Findings);
-   console.log("Tons of Rock:", Tons); 
-   console.log("skeikampenfestivalen:", Skei);
-   console.log("NEON:", Ne);
+   console.log("Festival:", Festival);
    console.log("city:", City);
  }, []);
 
@@ -79,38 +44,13 @@ export default function Home(){
         <>
         <h1>Sommerens festivaler!</h1>
         <section>
-        {Findings.map((fin) => (
-          <article key={fin.id}>
-            <img src={fin.images?.[0]?.url} alt={fin.name} />
-           <h3>{fin.name}</h3>
-           <button>Les mer om {fin.name}</button>
+        {Festival.map((fe) => (
+          <article key={fe.id}>
+            {<img src={fe.images?.[0]?.url} alt={fe.name} />}
+           <h3>{fe.name}</h3>
+           <button>Les mer om {fe.name}</button>
            </article>
-        ))}
-        
-
-        {Tons.map((ton) => (
-          <article key={ton.id}>
-            <img src={ton.images?.[0]?.url} alt={ton.name} />
-            <h3>{ton.name}</h3>
-            <button>Les mer om {ton.name}</button>
-            </article>
-        ))}
-
-        {Skei.map((skei) => (
-          <article key={skei.id}>
-            <img src={skei.images?.[0]?.url} alt={skei.name} />
-            <h3>{skei.name}</h3>
-            <button>Les mer om {skei.name}</button>
-          </article>
-        ))}
-
-        {Ne.map((ne) => (
-          <article key={ne.id}>
-            <img src={ne.images?.[0]?.url} alt={ne.name} />
-            <h3>{ne.name}</h3>
-            <button>Les mer om {ne.name}</button>
-          </article>
-        ))}
+        ))} 
         </section>
 
         <section className="Byer">
