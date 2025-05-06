de4c842a47c2c43eac8f0eedea9eaee028f4594f
nikolaiols <nikolaio@hiof.no>
2025-05-06 15:09:22 +0200

    lagd if setining som fjerner duplikatartister

--- Bilettlyst/src/components/ArtistCard.jsx
+++ Bilettlyst/src/components/ArtistCard.jsx
@@ -6,7 +6,7 @@ export default function ArtistCard(){
 
     const [ArtistCard, setArtistCard] = useState([]);
     const { slug } = useParams(); // Får slug fra URL
-    
+    let duplikatArtist = []
     const getArtistCard = async () => {
         fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=XiNPWWR7685AFoobg27DG2naIh92yDVH&keyword=${slug}&locale=*&countryCode=NO`) // Henter data fra API-et.
           .then((response) => response.json()) //gjør om til JSON-format
@@ -20,17 +20,24 @@ export default function ArtistCard(){
       }, []);
 
     return(
-         <section>
-          <h3>Artister</h3>
-         {ArtistCard.map((ev) => (
-           ev._embedded?.attractions?.map((artist) => (
-             <article key={artist.id}>
-               <h3>{artist.name}</h3>
-               <img src={artist.images?.[0]?.url} alt={artist.name} />
-             </article>
-           ))
-         ))}
-
-       </section>
+      <section>
+      <h3>Artister</h3>
+      {/*Siden det er flere versjoner av konsertene vil artistene printes ut flere ganger. Dette ble ungått ved å lage en if-test
+       som sjekker om id-en til artisten allerede er brukt*/}
+      {ArtistCard.map((ev) =>
+        ev._embedded?.attractions?.map((artist) => {
+          if (duplikatArtist.includes(artist.id)) {
+            return null;
+          }
+          duplikatArtist.push(artist.id);
+          return (
+            <article key={artist.id}>
+              <h3>{artist.name}</h3>
+              <img src={artist.images?.[0]?.url} alt={artist.name} />
+            </article>
+          );
+        })
+      )}
+    </section>
     )
 }
