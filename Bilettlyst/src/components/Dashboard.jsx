c4c65ea8a1ada9e868e2f6fb4aa275521339efb4
KristofferHoff <kristoffer.hoff@me.com>
2025-05-06 15:29:28 +0200

    oppdatert dashboard
    Laget dashboard siden til karakter C

--- Bilettlyst/src/components/Dashboard.jsx
+++ Bilettlyst/src/components/Dashboard.jsx
@@ -1,5 +1,41 @@
-export default function(){
+import React, { useState } from 'react';
+
+// Definerer Dashboard komponentet
+export default function Dashboard(){
+    // Oppretter en state-variabel, for å skjekke om brukeren er logget inn
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // Oppretter en state-variabel, for å lagre brukernavnet
+    const [username, setUsername] = useState('');
+    // Funksjon som håndterer innsending av loggin-skjema
+  const handleLogin = (e) => {
+    setIsLoggedIn(true); // Setter brukeren til, innlogget
+  };
+
     return(
-        <h1>Dashboard</h1> /* "Min side" med info om bruker og kjøp */
+        <section>
+      {!isLoggedIn ? (
+        <>
+          <h2>Logg inn</h2>
+          <form onSubmit={handleLogin}>
+            <label>
+              Navn:
+              <input
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </label>
+            <button type="submit">Logg inn</button>
+          </form>
+        </>
+
+        // Når brukeren logger inn, viser dette innholdet
+      ) : (
+        <>
+          <h2>Min side</h2>
+          <p>Velkommen, {username}!</p>
+        </>
+      )}
+    </section>
     )
 }
